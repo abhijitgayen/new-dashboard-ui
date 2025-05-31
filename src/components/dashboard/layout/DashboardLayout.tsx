a045a27e11b423a0f2fcc42ebeed388e79306e08
shuvajitg <shuvajitgayen57@gmail.com>
2025-05-31 09:55:39 +0530

    clean the code

--- src/components/dashboard/layout/DashboardLayout.tsx
+++ src/components/dashboard/layout/DashboardLayout.tsx
@@ -1,10 +1,8 @@
 import { ThemeProvider, useTheme } from "@/contexts/ThemeContext";
 import DashboardSidebar from "@/components/dashboard/DashboardSidebar";
-import AnalyticsWidgets from "@/components/dashboard/AnalyticsWidgets";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
-import { SidebarMode } from "@/contexts/ThemeContext";
 
 
 const DashboardContent = () => {
