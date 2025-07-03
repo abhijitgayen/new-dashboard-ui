6606f298b77a375fa82dad6f03272290787fdac1
shuvajitg <shuvajitgayen57@gmail.com>
2025-07-03 10:36:27 +0530

    feat: enhance AppearanceForm with dark-blue theme option and update theme context usage

--- src/components/settings/appearance/appearance-form.tsx
+++ src/components/settings/appearance/appearance-form.tsx
@@ -6,7 +6,7 @@ import { fonts } from '@/config/fonts'
 import { cn } from '@/lib/utils'
 import { showSubmittedData } from '@/utils/show-submitted-data'
 import { useFont } from '@/contexts/font-context'
-import { useTheme } from '@/contexts/theme-context'
+import { useTheme } from '@/contexts/ThemeContext'
 import { Button, buttonVariants } from '@/components/ui/button'
 import {
   Form,
@@ -20,7 +20,7 @@ import {
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 
 const appearanceFormSchema = z.object({
-  theme: z.enum(['light', 'dark'], {
+  theme: z.enum(['light', 'dark', 'dark-blue'], {
     required_error: 'Please select a theme.',
   }),
   // font: z.enum(fonts, {
@@ -33,11 +33,11 @@ type AppearanceFormValues = z.infer<typeof appearanceFormSchema>
 
 export function AppearanceForm() {
   // const { font, setFont } = useFont()
-  const { theme, setTheme } = useTheme()
+  const { settings, updateSettings } = useTheme()
 
   // This can come from your database or API.
   const defaultValues: Partial<AppearanceFormValues> = {
-    theme: theme as 'light' | 'dark',
+    theme: settings.colorMode as 'light' | 'dark' | "dark-blue",
     // font,
   }
 
@@ -48,7 +48,7 @@ export function AppearanceForm() {
 
   function onSubmit(data: AppearanceFormValues) {
     // if (data.font != font) setFont(data.font)
-    if (data.theme != theme) setTheme(data.theme)
+    if (data.theme != settings.colorMode) updateSettings({ colorMode: data.theme })
 
     showSubmittedData(data)
   }
@@ -101,7 +101,7 @@ export function AppearanceForm() {
               <RadioGroup
                 onValueChange={field.onChange}
                 defaultValue={field.value}
-                className='grid max-w-md grid-cols-2 gap-8 pt-2'
+                className='grid max-w-lg grid-cols-3 gap-8 pt-2'
               >
                 <FormItem>
                   <FormLabel className='[&:has([data-state=checked])>div]:border-primary'>
@@ -153,6 +153,29 @@ export function AppearanceForm() {
                     <span className='block w-full p-2 text-center font-normal'>
                       Dark
                     </span>
+                  </FormLabel>
+                </FormItem>
+                <FormItem>
+                  <FormLabel className='[&:has([data-state=checked])>div]:border-primary'>
+                    <FormControl>
+                      <RadioGroupItem value='dark-blue' className='sr-only' />
+                    </FormControl>
+                    <div className='border-muted bg-popover hover:bg-accent hover:text-accent-foreground items-center rounded-md border-2 p-1'>
+                      <div className='space-y-2 rounded-sm bg-[#1a212d] p-2'>
+                        <div className='space-y-2 rounded-md bg-blue-950 p-2 shadow-xs'>
+                          <div className='h-2 w-[80px] rounded-lg bg-blue-400' />
+                          <div className='h-2 w-[100px] rounded-lg bg-blue-400' />
+                        </div>
+                        <div className='flex items-center space-x-2 rounded-md bg-blue-950 p-2 shadow-xs'>
+                          <div className='h-4 w-4 rounded-full bg-blue-400' />
+                          <div className='h-2 w-[100px] rounded-lg bg-blue-400' />
+                        </div>
+                        <div className='flex items-center space-x-2 rounded-md bg-blue-950 p-2 shadow-xs'>
+                          <div className='h-4 w-4 rounded-full bg-blue-400' />
+                          <div className='h-2 w-[100px] rounded-lg bg-blue-400' />
+                        </div>
+                      </div>
+                    </div>
                   </FormLabel>
                 </FormItem>
               </RadioGroup>
